150f76312c4efbc466451ef0e2ce7f1621f68aae
shubh <ror.shubh@gmail.com>
2018-11-15 14:24:00 +0530

    removed comments

--- src/store/aeppsdk.js
+++ src/store/aeppsdk.js
@@ -1,25 +1,6 @@
 import Chain from '@aeternity/aepp-sdk/es/chain/epoch'
-/* import Tx from '@aeternity/aepp-sdk/es/tx/epoch'
-import Contract from '@aeternity/aepp-sdk/es/ae/contract'
-import Account from '@aeternity/aepp-sdk/es/account/memory' */
-
-// TODO: delete after SDK doesn't require a valid keyPair for Contrac
-// import { generateKeyPair } from '@aeternity/aepp-sdk/es/utils/crypto'
 
 let client = null
-
-/* const AeClient = Chain
- .compose(Tx)
-.compose(Contract)
- .compose(Account)
- .debugSwagger(true)
-
-const ae = function (url = process.env.VUE_APP_EPOCH_URL) {
-  return AeClient({
-    url,
-    keypair: generateKeyPair()
-  })
-} */
 
 /**
  * getEpochClient
@@ -40,5 +21,3 @@ export const getEpochClient = async (epochUrl = process.env.VUE_APP_EPOCH_URL) =
 export const resetClient = () => {
   client = null
 }
-
-// export default ae
